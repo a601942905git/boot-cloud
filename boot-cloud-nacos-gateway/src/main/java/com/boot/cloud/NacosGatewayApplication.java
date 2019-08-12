451573653f175e1d24398e53b8d584fb6b0d5cf3
lipeng <lipeng@shein.com>
2019-08-12 14:01:59 +0800

    新增通过网关访问服务

---
+++ boot-cloud-nacos-gateway/src/main/java/com/boot/cloud/NacosGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.boot.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * com.boot.cloud.NacosGatewayApplication
+ *
+ * @author lipeng
+ * @date 2019-08-12 13:31
+ */
+@SpringBootApplication
+public class NacosGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosGatewayApplication.class, args);
+    }
+}
