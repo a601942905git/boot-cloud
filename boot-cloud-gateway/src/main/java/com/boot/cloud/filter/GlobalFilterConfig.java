17991b81c98d4b50c11d39d81511cd77f0f9abdf
lipeng <lipeng@shein.com>
2019-09-20 10:38:40 +0800

    新增全局过滤器配置

---
+++ boot-cloud-gateway/src/main/java/com/boot/cloud/filter/GlobalFilterConfig.java
@@ -0,0 +1,53 @@
+package com.boot.cloud.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import reactor.core.publisher.Mono;
+
+/**
+ * com.boot.cloud.filter.GlobalFilterConfig
+ * 定义全局过滤器
+ * 需要越低，优先级越高，优先级高的pre先执行，post最后执行
+ * @author lipeng
+ * @date 2019-09-19 17:04
+ */
+@Configuration
+@Slf4j
+public class GlobalFilterConfig {
+
+    @Bean
+    @Order(-1)
+    public GlobalFilter a() {
+        return (exchange, chain) -> {
+            log.info("first pre filter");
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                log.info("first post filter");
+            }));
+        };
+    }
+
+    @Bean
+    @Order(0)
+    public GlobalFilter b() {
+        return (exchange, chain) -> {
+            log.info("second pre filter");
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                log.info("second post filter");
+            }));
+        };
+    }
+
+    @Bean
+    @Order(1)
+    public GlobalFilter c() {
+        return (exchange, chain) -> {
+            log.info("third pre filter");
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                log.info("third post filter");
+            }));
+        };
+    }
+}
