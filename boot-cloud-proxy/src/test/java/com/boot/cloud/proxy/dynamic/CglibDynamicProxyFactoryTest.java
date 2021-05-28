aa584409d021ca8121d7f7e8147f7c56a42076c6
lipeng <lipeng@shein.com>
2021-05-28 17:53:41 +0800

    cglib代理实现方式

--- boot-cloud-proxy/src/test/java/com/boot/cloud/proxy/dynamic/CglibDynamicProxyFactoryTest.java
+++ boot-cloud-proxy/src/test/java/com/boot/cloud/proxy/dynamic/CglibDynamicProxyFactoryTest.java
@@ -2,7 +2,11 @@ package com.boot.cloud.proxy.dynamic;
 
 import com.boot.cloud.proxy.HelloService;
 import com.boot.cloud.proxy.HelloServiceImpl;
+import com.boot.cloud.proxy.UserService;
 import org.junit.Test;
+import org.springframework.cglib.core.DebuggingClassWriter;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
 
 /**
  * com.boot.cloud.proxy.dynamic.CglibDynamicProxyFactoryTest
@@ -18,5 +22,28 @@ public class CglibDynamicProxyFactoryTest {
         CglibDynamicProxyFactory proxyFactory = new CglibDynamicProxyFactory(cglibDynamicProxy);
         HelloService proxy = (HelloService) proxyFactory.getProxy();
         proxy.hello();
+    }
+
+    @Test
+    public void test1() {
+
+        System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, "/Users/pengli/software/idea");
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(UserService.class);
+        /**
+         * target：生成的代理对象
+         * method：被代理对象方法
+         * arguments：被代理对象方法参数
+         * methodProxy：代理方法
+         */
+        enhancer.setCallback((MethodInterceptor) (target, method, arguments, methodProxy) -> {
+            System.out.println("方法执行前");
+            Object result = methodProxy.invokeSuper(target, arguments);
+            System.out.println("方法执行后");
+            return result;
+        });
+        // 生成的代理类会继承被代理类，并重写父类中非final修饰的方法并在方法中通过super.xxx()调用父类方法
+        UserService userService = (UserService) enhancer.create();
+        userService.say();
     }
 }
