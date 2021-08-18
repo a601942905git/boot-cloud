f2be5731edc3452309aed3c30985f087f47c87e3
a601942905@163.com <lipeng@shein.com>
2021-08-18 19:27:26 +0800

    全链路示例添加注释

--- boot-cloud-spring/boot-cloud-opentracing/boot-cloud-opentracing-consumer/src/main/java/com/boot/cloud/OpentracingConsumerApplication.java
+++ boot-cloud-spring/boot-cloud-opentracing/boot-cloud-opentracing-consumer/src/main/java/com/boot/cloud/OpentracingConsumerApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * com.boot.cloud.OpentracingConsumerApplication
  *
+ * @see io.opentracing.contrib.spring.web.starter.RestTemplateTracingAutoConfiguration
+ * 注入RestTemplate，设置TracingRestTemplateInterceptor拦截器
+ * 在执行http前调用TracingRestTemplateInterceptor的intercept()注入全链路信息
+ *
  * @see io.opentracing.contrib.spring.web.client.TracingRestTemplateInterceptor 注入
  *
  * @see io.opentracing.contrib.web.servlet.filter.TracingFilter 提取
