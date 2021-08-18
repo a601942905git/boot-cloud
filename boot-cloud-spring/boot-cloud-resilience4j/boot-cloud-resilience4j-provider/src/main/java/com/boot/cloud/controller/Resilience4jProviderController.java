f4516bf501c32789f3ce7c281012f6cf8e694526
a601942905@163.com <lipeng@shein.com>
2021-08-18 19:28:46 +0800

    SpringCloud集成Resilience4j

---
+++ boot-cloud-spring/boot-cloud-resilience4j/boot-cloud-resilience4j-provider/src/main/java/com/boot/cloud/controller/Resilience4jProviderController.java
@@ -0,0 +1,13 @@
+package com.boot.cloud.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * com.boot.cloud.controller.Resilience4jController
+ *
+ * @author lipeng
+ * @date 2021/8/18 6:07 PM
+ */
+@RestController
+public class Resilience4jProviderController {
+}
