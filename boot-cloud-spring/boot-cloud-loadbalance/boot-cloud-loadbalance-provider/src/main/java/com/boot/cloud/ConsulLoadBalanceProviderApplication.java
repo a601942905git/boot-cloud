3d25e6b7729750687ce70c21939bf2e6a2fefd10
a601942905@163.com <lipeng@shein.com>
2021-08-25 14:34:04 +0800

    添加注释

--- boot-cloud-spring/boot-cloud-loadbalance/boot-cloud-loadbalance-provider/src/main/java/com/boot/cloud/ConsulLoadBalanceProviderApplication.java
+++ boot-cloud-spring/boot-cloud-loadbalance/boot-cloud-loadbalance-provider/src/main/java/com/boot/cloud/ConsulLoadBalanceProviderApplication.java
@@ -7,6 +7,16 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 /**
  * com.boot.cloud.LoadBalanceApplication
  *
+ * 服务注册流程
+ * @see org.springframework.cloud.consul.serviceregistry.ConsulAutoServiceRegistrationListener 实现
+ * @see org.springframework.context.event.SmartApplicationListener 接口，应用启动会调用onApplicationEvent()方法
+ *
+ * @see org.springframework.cloud.consul.serviceregistry.ConsulAutoServiceRegistration
+ *      @see org.springframework.cloud.consul.serviceregistry.ConsulServiceRegistry 服务注册
+ *          @see com.ecwid.consul.v1.agent.AgentConsulClient 调用/v1/agent/service/register接口注册服务
+ *  @see org.springframework.cloud.consul.serviceregistry.ConsulAutoRegistration 注册表，含有要注册服务信息
+ *
+ *
  * @author lipeng
  * @date 2021/8/18 7:35 PM
  */
